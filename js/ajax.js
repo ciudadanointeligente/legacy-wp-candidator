581499cf390fc7f43d0d41b507dc067b57ec86b1
jdgarrido <damian.garrido.munoz@gmail.com>
2013-06-26 13:09:44 -0400

    mejoras js
    mejoras menores de javascript para la seleccion y comparacion de
    candidatos

--- js/ajax.js
+++ js/ajax.js
@@ -6,13 +6,23 @@ function comparar(){
         });
 }
 
+function inArray(needle, haystack) {
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+        if(haystack[i] == needle) return true;
+    }
+    return false;
+}
+
 jQuery(document).ready(function($) {
-    $("input:checkbox").on('click', function(){
-        var numberOfChecked = $('input:checkbox:checked').length;
+    $("input[type=checkbox]").on('click', function(){
+        $(this).attr('disabled','disabled');
+        var numberOfChecked = $('input[type=checkbox]:checked').length;
         if(numberOfChecked<=2) {
             var candidate_name = $(this).data('candidate-name');
             var candidate_id = $(this).val();
-            $(".compare ul").append('<li data-lolo="'+candidate_id+'">'+candidate_name+'<a class="remove-candidate" href="#" data-candidate-id="'+candidate_id+'">x</a></li>');
+            
+            $(".compare ul").append('<li data-id="'+candidate_id+'">'+candidate_name+' <a class="remove-candidate" href="#" data-candidate-id="'+candidate_id+'">x</a></li>');
             
             if(numberOfChecked==2) {
                 $(".compare").append('<input type="button" name="comparar" value="Comparar" onClick="comparar()">');
@@ -23,9 +33,11 @@ jQuery(document).ready(function($) {
     $(".remove-candidate").live('click', function(event){
         event.preventDefault();
         var candidate_id = $(this).data('candidate-id');
+        
         $(this).parent().remove();
         $("input[name=candidato-"+candidate_id+"]").attr('checked', false);
-        
+        $("input[name=candidato-"+candidate_id+"]").removeAttr('disabled');
+
         var numberOfChecked = $('input:checkbox:checked').length;
         if(numberOfChecked<2) {
             $("input[name=comparar]").remove();
